c38968fb70b6f861b92f97c5944d618a08fcf07d
althaf.azard <althaf.azard@randoli.ca>
2023-06-09 11:03:51 +0530

    First commit

---
+++ Back-end/src/main/java/project/edublink/EdublinkApplication.java
@@ -0,0 +1,13 @@
+package project.edublink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdublinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdublinkApplication.class, args);
+	}
+
+}
