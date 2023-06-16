1e4aa52f2febd20c7648dbc3ef554b2f26713753
althaf.azard <althaf.azard@randoli.ca>
2023-06-16 05:33:45 +0530

    First commit on frontend

---
+++ Frontend/src/components/checkout/order-payment.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import Link from "next/link";
+
+const OrderPayment = () => {
+  const handleClick = () => {
+    console.log("Test");
+  };
+
+  return (
+    <div className="order-payment">
+      <h4 className="title">Payment</h4>
+      <div className="payment-method">
+        <div className="form-group">
+          <div className="edu-form-check">
+            <input type="radio" id="pay-bank" name="payment" />
+            <label htmlFor="pay-bank">Direct Bank Transfer</label>
+          </div>
+          <p>
+            Please send a check to Store Name, Store Street, Store Town, Store
+            State / County, Store Postcode.
+          </p>
+        </div>
+
+        <div className="form-group">
+          <div className="edu-form-check">
+            <input type="radio" id="pay-pal" name="payment" />
+            <label htmlFor="pay-pal">PayPal</label>
+            <a href="#">What is Paypal?</a>
+          </div>
+        </div>
+      </div>
+
+      {/* <a href="#Test" className="edu-btn order-place">
+        Place Your order <i className="icon-4"></i>
+      </a> */}
+      <div>
+        <button onClick={handleClick}>Likes </button>
+      </div>
+    </div>
+  );
+};
+
+export default OrderPayment;
